d4c984c43e5e6236944c2d937c461a224a7f2e3c
Glitch (shimmering-radial-hen) <none>
2021-05-20 18:03:44 +0000

    👡🌎 Checkpoint ./server.js:24539575/7

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ client.on("ready", () => {
 client.on("warn", info => console.log(info));
 client.on("error", console.error);
 
-//////
+/////////////
 client.on("guildCreate", guild => { let channel = client.channels.cache.get("838149297362305084"); let embed = new MessageEmbed().setColor("#FF0000") .setAuthor(client.user.username, client.user.avatarURL()) .setTitle( `✅ Join Server`) .addField(" **Server Name**", `${guild.name}`) .addField(" **Server Owner**", `${guild.owner}`) .addField(" **Server Id**", `${guild.id}`) .addField(" **Member Count**", `${guild.memberCount}`) .setFooter(`${client.user.tag}`); channel.send(embed);}); client.on("guildDelete", guild => { let channel = client.channels.cache.get("838149298474057808"); let embed = new MessageEmbed() .setColor("#FF0000") .setAuthor(client.user.username, client.user.avatarURL()) .setTitle( `❌ Left Server`) .addField(" **Server Name**", `${guild.name}`) .addField(" **Server Owner**", `${guild.owner}`) .addField(" **Server Id**", `${guild.id}`) .addField(" **Member Count**", `${guild.memberCount}`) .setFooter(`${client.user.tag}`); channel.send(embed);});
 //////
 
